f606ea240bf41d12a6b605800c5ba7e498bee9f1
zhouyangyang <1453238318@qq.com>
2026-09-14 14:35:13 +0800

    实验结果命名

--- tools/training_tools.py
+++ tools/training_tools.py
@@ -18,6 +18,12 @@ def setup_seed(seed):
 
 def setup_paths(args):
     save_root = args.save_path
+
+    if isinstance(args.training_data, (list, tuple)):
+        training_data_name = '-'.join(str(name) for name in args.training_data)
+    else:
+        training_data_name = str(args.training_data)
+
     model_root = os.path.join(save_root, 'models')
     log_root = os.path.join(save_root, 'logs')
     csv_root = os.path.join(save_root, 'csvs')
@@ -32,15 +38,23 @@ def setup_paths(args):
 
     if args.use_hsf:
         # prepare model name
-        model_name = f'{args.exp_indx}s-pretrained-{args.training_data}-{args.model}-' \
+        model_name = f'{args.exp_indx}s-pretrained-{training_data_name}-{args.model}-' \
                      f'{args.prompting_type}-{args.prompting_branch}-' \
                      f'D{args.prompting_depth}-L{args.prompting_length}-HSF-K{args.k_clusters}'
     else:
         # prepare model name
-        model_name = f'{args.exp_indx}s-pretrained-{args.training_data}-{args.model}-' \
+        model_name = f'{args.exp_indx}s-pretrained-{training_data_name}-{args.model}-' \
                      f'{args.prompting_type}-{args.prompting_branch}-' \
                      f'D{args.prompting_depth}-L{args.prompting_length}-WO-HSF'
 
+    model_name += (
+        f'-F{args.fusion_mode}'
+        f'-W{args.static_warmup_epochs}'
+        f'-S{args.seed}'
+    )
+
+    if args.fusion_mode == 'residual_layer_gate':
+        model_name += f'-M{args.prompt_ema_momentum:g}'
 
     # prepare model path
     ckp_path = os.path.join(model_root, model_name)
@@ -63,5 +77,4 @@ def setup_paths(args):
     log_path = os.path.join(log_root, f'{model_name}-{args.testing_data}.txt')
 
     return model_name, image_dir, csv_path, log_path, ckp_path, tensorboard_logger
-
 
